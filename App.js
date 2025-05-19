74d38b006ecaf6d8ddf00a752d83756989f08fef
twoh365 <thomaswilliamsohara@me.com>
2025-05-19 08:15:17 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from "react";
+import StaycityMenuCalculator from "./StaycityMenuCalculator";
+
+function App() {
+    return (
+        <div>
+            <StaycityMenuCalculator />
+        </div>
+    );
+}
+
+export default App;
